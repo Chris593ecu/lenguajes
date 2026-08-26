19bd35a18a3037b6c6aa5463c8b9bbdbfaca8dd1
DistribuidoraELFABI <distribuidoraalpos@hotmail.com>
2026-08-25 21:28:33 -0500

    vida silvestre

--- javascript/ejercicios/rastraedor-vida-silvestre/vida-silvestre.js
+++ javascript/ejercicios/rastraedor-vida-silvestre/vida-silvestre.js
@@ -33,3 +33,31 @@ function addHabitat(animal, habitat) {
     return animal;
 }
 console.log(addHabitat(tiger, 'Rainforest'));
+
+const updateAge = (animal, newAge) => {
+    animal.age = newAge;
+    return animal;
+};
+
+console.log(updateAge(elephant, 12));
+
+const removeEndangeredStatus = (animal) => {
+    delete animal.isEndangered;
+    return animal;
+};
+
+console.log(removeEndangeredStatus(tiger));
+
+const hasHabitat = (animal) => animal.hasOwnProperty('habitat');
+
+console.log(hasHabitat(tiger));
+console.log(hasHabitat(elephant));
+
+// function getProperty(animal, propertyName) {
+//     return animal[propertyName];
+// }
+
+const getProperty = (animal, propertyName) => animal[propertyName];
+
+console.log(getProperty(tiger, 'species'));
+console.log(getProperty(elephant, 'age'));
