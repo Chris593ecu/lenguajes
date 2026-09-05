b650c085bf9cff3541afb75f1f68e2bf4cb3f870
DistribuidoraELFABI <distribuidoraalpos@hotmail.com>
2026-09-05 13:33:38 -0500

    Algotitmo Chunky Monkey

---
+++ javascript/ejercicios/bucles/algoritmo-chunky-monkey.js
@@ -0,0 +1,11 @@
+// #Algotitmo Chunky Monkey
+
+function chunkArrayInGroups(array, num) {
+    const result = [];
+
+    for (let i = 0; i < array.length; i += num) {
+        result.push(array.slice(i, i + num));
+    }
+
+    return result;
+}
